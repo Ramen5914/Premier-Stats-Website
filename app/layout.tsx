f0bc1ade2182e698482307e69cbaa88536f5fa55
Ramen5914 <Ramen5914@gmail.com>
2023-10-06 01:44:22 -0700

    Added header and footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './global.css'
+import Navbar from './(components)/Navbar'
+import Footer from './(components)/Footer'
 
 export default function RootLayout({
     children,
@@ -6,8 +8,16 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en-US">
+            <body className='flex flex-col min-h-screen'>
+                <Navbar />
+                <main>
+                    <div className="container mx-xl grow">
+                        {children}
+                    </div>
+                </main>
+                <Footer />
+            </body>
         </html>
     )
 }
