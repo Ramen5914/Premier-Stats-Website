137010aed472f395146125214a5717152b72a36c
Ramen5914 <Ramen5914@gmail.com>
2023-10-13 09:32:47 -0700

    Added links

--- app/(components)/TeamCard.tsx
+++ app/(components)/TeamCard.tsx
@@ -1,9 +1,13 @@
-export default function TeamCard({id}: {id: number}) {
+import Link from 'next/link'
+
+export default function TeamCard({ id }: { id: number }) {
     const order = "order-[" + id + "]"
-    
+
     return (
-        <div key={id}>
-            <h1>Test {id}</h1>
-        </div>
+        <Link href={"/edit/team/" + id}>
+            <div key={id}>
+                <h1>Test {id}</h1>
+            </div>
+        </Link>
     )
 }
