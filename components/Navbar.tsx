c9eef6f188f825a8af6515758411680c80137e8c
Ramen5914 <Ramen5914@gmail.com>
2023-11-08 01:18:15 -0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,13 +7,16 @@ import { TeamPath } from "@/app/(types)/stringmatchers";
 
 export default function Navbar() {
     const pathname = usePathname();
-    var redirect: [string, string] = ["", ""]
+    var extra: boolean = false;
+    var redirect: string[] = []
     if (pathname == "/") {
         redirect[0] = '/add/team';
         redirect[1] = 'Add Team'
     } else if (pathname as TeamPath) {
         redirect[0] = pathname + '/add/player';
         redirect[1] = 'Add Player'
+        redirect[2] = pathname + '/add/match'
+        extra = true
     }
 
     return (
@@ -38,11 +41,17 @@ export default function Navbar() {
                         </div>
                     </div>
                     <div className="flex items-center">
-                        <div className="flex-shrink-0">
+                        <div className="flex-shrink-0 flex-row flex space-x-2">
                             <Link href={redirect[0]} type="button" className="text-white relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold shadow-sm">
                                 <svg className="-ml-0.5 w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
                                 {redirect[1]}
                             </Link>
+                            {extra && 
+                                <Link href={redirect[2]} type="button" className="text-white relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold shadow-sm">
+                                    <svg className="-ml-0.5 w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+                                    Add Match
+                                </Link>
+                            }
                         </div>
                     </div>
                 </div>
