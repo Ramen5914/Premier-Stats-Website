19afad35a4ae103a985febe4183912a01eabd451
Ramen5914 <Ramen5914@gmail.com>
2023-11-07 16:53:56 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import './(css)/global.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import '@/app/(css)/global.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
