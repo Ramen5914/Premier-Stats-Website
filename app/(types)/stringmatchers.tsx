04b7eea44a85f1a04791900197271a63527abb2f
Ramen5914 <Ramen5914@gmail.com>
2023-11-07 16:53:48 -0800

    Create stringmatchers.tsx

---
+++ app/(types)/stringmatchers.tsx
@@ -0,0 +1,4 @@
+export type TeamPath = `/team/${number}`
+export type PlayerPath = `/team/${number}/player/${number}`
+export type TeamMatchPath = `/team/${number}/match/${number}`
+export type PlayerMatchPath = `/team/${number}/player/${number}/match/${number}`
