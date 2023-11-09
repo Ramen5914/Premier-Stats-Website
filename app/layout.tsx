332a7e808cbf26d7470b0dba42b39a7f07b3bb25
Ramen5914 <Ramen5914@gmail.com>
2023-11-08 19:42:15 -0800

    format changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import '@/app/(css)/global.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
     return (
         <html lang="en-US">
             <body className="text-slate-400 bg-white dark:text-slate-500 dark:bg-slate-800 dark">
