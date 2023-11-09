514f235844c6c2b4d873f394a48c718d024ddc5c
Ramen5914 <Ramen5914@gmail.com>
2023-11-08 19:42:00 -0800

    Added a new icon to nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,12 @@ import { TeamPath } from "@/app/(types)/stringmatchers";
 export default function Navbar() {
     const pathname = usePathname();
     var extra: boolean = false;
+    var trash: boolean = true;
     var redirect: string[] = []
     if (pathname == "/") {
         redirect[0] = '/add/team';
         redirect[1] = 'Add Team'
+        trash = false;
     } else if (pathname as TeamPath) {
         redirect[0] = pathname + '/add/player';
         redirect[1] = 'Add Player'
@@ -50,6 +52,13 @@ export default function Navbar() {
                                 <Link href={redirect[2]} type="button" className="text-white relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold shadow-sm">
                                     <svg className="-ml-0.5 w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
                                     Add Match
+                                </Link>
+                            }
+                            {trash &&
+                                <Link href={pathname + "/delete"} type="button" className="text-white relative inline-flex items-center gap-x-1.5 rounded-md bg-red-600 px-2 py-2 text-sm font-semibold shadow-sm">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                    </svg>
                                 </Link>
                             }
                         </div>
