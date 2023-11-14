c97306dd1dcabc1f3b1be1802fae4b3b46f610c9
Ramen5914 <Ramen5914@gmail.com>
2023-11-14 02:34:41 -0800

    Added new round record and win record field to team card

--- app/team/[teamid]/@team/page.tsx
+++ app/team/[teamid]/@team/page.tsx
@@ -1,5 +1,6 @@
 import { Team } from "@/app/(types)/GraphQLStructures";
 import Image from 'next/image'
+import Link from "next/link";
 
 type Props = {
     params: { teamid: number }
@@ -15,6 +16,21 @@ export default async function Team({ params }: Props) {
 
 async function teamCardRenderer(teamId: number) {
     let team: Team = await getTeam(teamId);
+
+    var wins: number = 0;
+    var losses: number = 0;
+    var wonRounds: number = 0;
+    var lostRounds: number = 0;
+
+    for (let teamMatch of team.teamMatches) {
+        if (teamMatch.score == 100) {
+            wins += 1;
+        } else if (teamMatch.score == 25) {
+            losses += 1;
+        }
+        wonRounds += teamMatch.teamScore;
+        lostRounds += teamMatch.enemyScore;
+    }
 
     const qualifyReq: number = 600;
     const divisionSize: number = 56;
@@ -41,17 +57,31 @@ async function teamCardRenderer(teamId: number) {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row items-center space-x-2">
+            <div className="flex flex-row items-center space-x-2 justify-between">
                 <h1 className="text-3xl flex-row flex items-center">
                     {team.name}
                     <span className='ml-2 text-xl font-normal text-white bg-indigo-500 px-2 py-1 rounded-lg'>#{team.tag}</span>
                 </h1>
+                <Link className="p-2 rounded-md bg-slate-800 hover:bg-slate-700 duration-300" href={team.link}>
+                    <svg className="h-4 dark:text-white" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 189.177 64.563" xmlSpace="preserve">
+                        <g transform="translate(-40.878 -103.185)">
+                            <g fillOpacity="1" transform="translate(-.132 13.232)">
+                                <path fill="#e44020" d="M184.415 134.607v-19.91l13.272 19.91 13.272 19.91h19.228V89.958h-17.462v19.05c0 10.478-.09 19.047-.198 19.043-.11-.004-5.903-8.577-12.876-19.05l-12.676-19.043h-20.288v64.559h17.728z" display="inline" />
+                                <path fill="currentColor" d="M79.11 130.44v-24.078H99.483v48.155h17.727v-48.155h28.84v8.462l-12.766.069-12.766.068-.07 7.866-.07 7.866 5.482.072 5.482.071 6.324 11.84 6.323 11.84H163.226l-1.03-1.918c-.567-1.055-3.415-6.38-6.33-11.835l-5.298-9.916 1.992-.337c5.426-.916 9.627-5.179 10.441-10.594.182-1.213.254-5.226.193-10.848 0-7.46.342-19.11-10.939-18.977l-54.822-.069-56.423-.068v16.409H61.383v48.154H79.11z" display="inline" />
+                            </g>
+                        </g>
+                    </svg>
+                </Link>
+                
             </div>
             <div className="flex flex-row items-center space-x-2">
                 <label className="text-lg">{team.score}/600</label>
                 <div className="bg-slate-700 h-5 rounded-lg" style={{width: "100%"}}>
                     <div style={{ width: barFill, height: "100%" }} className="bg-amber-400 rounded-lg" />
                 </div>
+            </div>
+            <div>
+                Record: {wins}-{losses} | Rounds: {wonRounds}-{lostRounds}
             </div>
         </div>
     )
@@ -77,8 +107,11 @@ async function getTeam(teamid: number): Promise<Team> {
                         link
                         imageLink
                         region
-                        playerCount
-                        teamMatchCount
+                        teamMatches {
+                            score
+                            teamScore
+                            enemyScore
+                        }
                     }
                 }   
             `
