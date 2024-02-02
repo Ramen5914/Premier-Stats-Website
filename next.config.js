20371ce560d3863ec7d08576b670aaadfb8e1332
Ramen5914 <Ramen5914@gmail.com>
2024-02-02 01:28:36 -0800

    prettier format

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "imgsvc.trackercdn.com",
-        port: "",
-        pathname: "/url/**",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "imgsvc.trackercdn.com",
+                port: "",
+                pathname: "/url/**",
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
