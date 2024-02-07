50a1e4a5fd071af73e2f7f94a8ba0065b924ea2c
Ramen5914 <Ramen5914@gmail.com>
2024-02-07 14:50:35 -0800

    Changed postcss config style

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+        require("postcss-nested"),
+    ],
 };
+
+export default config;
