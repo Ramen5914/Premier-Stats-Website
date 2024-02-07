2ab8d5709d1aa30b2c96c3d887d3732b0fa9204c
Ramen5914 <Ramen5914@gmail.com>
2024-02-07 09:45:33 -0800

    Added constants for weighting params

--- functions/rps.tsx
+++ functions/rps.tsx
@@ -1,3 +1,26 @@
+const BEZIER_PRECISION = 4;
+
+const PLACEMENT_W = 50;
+const TRACKER_NETWORK_W = 250;
+const COMBAT_SCORE_W = 0;
+const KILLS_W = 0;
+const DEATHS_W = 0;
+const ASSISTS_W = 0;
+const KD_RATIO_W = 0;
+const PLUS_MINUS_W = 0;
+const DAMAGE_DELTA_W = 0;
+const AVERAGE_DAMAGE_W = 0;
+const HEADSHOT_W = 500;
+const KAST_W = 0;
+const FIRST_KILLS_W = 0;
+const FIRST_DEATHS_W = 0;
+const MULTI_KILLS_W = 0;
+
+const THREE_K_W = 0;
+const FOUR_K_W = 0;
+const FIVE_K_W = 0;
+const SIX_K_W = 0;
+
     place: number,
     tns: number,
     acs: number,
