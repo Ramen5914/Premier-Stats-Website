5a37720a83124711a2bf3262b73884b3cac15a93
Ramen5914 <Ramen5914@gmail.com>
2024-02-08 01:12:32 -0800

    Fixed postcss issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-    plugins: [
-        require("tailwindcss"),
-        require("autoprefixer"),
-        require("postcss-nested"),
-    ],
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
-
-export default config;
