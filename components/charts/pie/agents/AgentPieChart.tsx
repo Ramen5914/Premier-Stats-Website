43a5397c47715b37c0a69ca8f8f3cc0bf8cbdb83
Ramen5914 <Ramen5914@gmail.com>
2024-02-15 09:31:45 -0800

    Fixed pie charts

--- components/charts/pie/agents/AgentPieChart.tsx
+++ components/charts/pie/agents/AgentPieChart.tsx
@@ -7,18 +7,50 @@ import { PlayerMatch } from "./agentPieSchema";
 export default function AgentPieChart({
     playerMatches,
 }: Readonly<{ playerMatches: PlayerMatch[] }>) {
-    console.log(playerMatches);
+    const agentCounter: { [id: string]: [number, string] } = {
+        Astra: [0, "#7E24D6"],
+        Breach: [0, "#8B4C31"],
+        Brimstone: [0, "#A52F17"],
+        Chamber: [0, "#FAA51C"],
+        Cypher: [0, "#5480C2"],
+        Deadlock: [0, "#CEB89D"],
+        Fade: [0, "#344261"],
+        Gekko: [0, "#B2D360"],
+        Harbor: [0, "#5591BE"],
+        Iso: [0, "#8A5EF0"],
+        Jett: [0, "#B7D2DA"],
+        KAY_O: [0, "#13208F"],
+        Killjoy: [0, "#F5CE2C"],
+        Neon: [0, "#423DD6"],
+        Omen: [0, "#393455"],
+        Phoenix: [0, "#E58B47"],
+        Raze: [0, "#BE4D2F"],
+        Reyna: [0, "#7B3675"],
+        Sage: [0, "#3EB1A9"],
+        Skye: [0, "#628C66"],
+        Sova: [0, "#49CBDF"],
+        Viper: [0, "#63BF4A"],
+        Yoru: [0, "#2221B4"],
+    };
+
+    // const agentSet: Set<string> = new Set<string>();
+
+    for (let match of playerMatches) {
+        agentCounter[match.agent][0]++;
+    }
+
+    let data = getDataPoints(agentCounter);
 
     return (
         <Pie
             datasetIdKey='agentPieChart'
             data={{
-                labels: ["Reyna", "Jett", "KAY/O"],
+                labels: data[0],
                 datasets: [
                     {
                         label: "Times Picked",
-                        data: [5, 2, 1],
-                        backgroundColor: ["#CA00D9", "#91DAFF", "#2f5af3"],
+                        data: data[1],
+                        backgroundColor: data[2],
                         borderWidth: 1,
                     },
                 ],
@@ -43,4 +75,21 @@ export default function AgentPieChart({
             }}
         />
     );
+}
+
+function getDataPoints(agentCounter: {
+    [key: string]: [number, string];
+}): [string[], number[], string[]] {
+    let agents: string[] = [];
+    let data: number[] = [];
+    let color: string[] = [];
+    for (let key in agentCounter) {
+        if (agentCounter[key][0] > 0) {
+            agents.push(key);
+            data.push(agentCounter[key][0]);
+            color.push(agentCounter[key][1]);
+        }
+    }
+
+    return [agents, data, color];
 }
